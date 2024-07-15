a2315c315a19c790016ba67b8a1b40456b3b09c1
Arnold-In-Tech <g16a7782@gmail.com>
2024-07-16 01:29:23 +0300

    completed assignment

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,19 @@
 class Course:
-    pass
+    def __init__(self, title="", schedule="", description=""):  # initialize default ("") values to pass the test
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+        
+    def __str__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output
+
+# To use the Courses class above:
+    
+# def main():
+#     course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+#     print(course)
+
+# main()
--- lib/Scraper.py
+++ lib/Scraper.py
@@ -4,5 +4,38 @@ from Course import Course
 import ipdb
 
 
+# ======= SOLUTION CODE : Scraper.py
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        # more code coming soon!
+        doc = BeautifulSoup(requests.get(
+            "http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+        # ipdb.set_trace()
+        return doc
+
+    def get_courses(self):
+        return self.get_page().select('.post')
+
+    def make_courses(self):
+        for course in self.get_courses():
+
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            date = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text if course.select("p") else ''
+
+            new_course = Course(title, date, description)
+            self.courses.append(new_course)
+        return self.courses
+
+    def print_courses(self):
+        for course in self.make_courses():
+            print(course)
+
+# ======= 
+
+# To use the Scraper class above:
+# s = Scraper()         # first create an instance
+# s.print_courses()
